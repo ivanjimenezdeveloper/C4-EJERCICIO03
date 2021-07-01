bd37e49b11b2204f2372f4ae109cd42d1f46cbe2
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-07-01 13:47:02 +0200

    mensaje de error listo

--- utilidades/archivos.js
+++ utilidades/archivos.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+const chalk = require("chalk");
 const guardarSaludo = (nombre) => {
   const ruta = path.join("./textos", "saludo.txt");
   fs.writeFile(ruta, `Hola, ${nombre} Encantado de saludarte`, (err) => {
-    if (err) console.log(err.message);
+    if (err) console.log(chalk.red.bold(err.message));
   });
 };
 
